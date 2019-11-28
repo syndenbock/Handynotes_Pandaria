b3e187ae6e3dabc67527ed2b434161b9c47f9dfd
semecan <semecan>
2019-11-28 16:21:08 +0100

    Add settings support with settings for icon scale and alpha

--- Handynotes_Pandaria/modules/handynotes_integration.lua
+++ Handynotes_Pandaria/modules/handynotes_integration.lua
@@ -5,6 +5,7 @@ local HandyNotes = shared.HandyNotes;
 local nodes = shared.nodes;
 local nodeInfo = {};
 local handler = {};
+local settings;
 local tooltip;
 
 local function makeIterator (zones, isMinimap)
@@ -30,7 +31,7 @@ local function makeIterator (zones, isMinimap)
             nodeInfo[zone][coords] = info;
 
             if (info.display) then
-              return coords, zone, info.icon, 1, 1;
+              return coords, zone, info.icon, settings.icon_scale, settings.icon_alpha;
             end
           end
 
@@ -108,47 +109,91 @@ function handler:OnLeave(uiMapId, coords)
   tooltip:Hide();
 end
 
-local db = {};
+local function replaceTable (oldTable, newTable)
+  -- this clears the table without destroying old references
+  table.wipe(oldTable);
 
-local options = {
-  type = "group",
-  name = 'Handynotes Pandaria',
-  desc = 'Handynotes Pandaria',
-  get = function(info) return db[info.arg] end,
-  set = function(info, v)
-    db[info.arg] = v
-  end,
-  args = {
-    desc = {
-      name = 'Handynotes Pandaria Options.',
-      type = "description",
-      order = 0,
+  for key, value in pairs(newTable) do
+    oldTable[key] = value;
+  end
+end
+
+local function validateSettings (config, defaults)
+  local new = {};
+
+  for param, defaultValue in pairs(defaults) do
+    local configValue = config[param];
+
+    if (type(configValue) ~= type(defaultValue)) then
+      new[param] = defaultValue;
+    else
+      new[param] = configValue;
+    end
+  end
+
+  -- we replace the old settings table to wipe removed settings
+  replaceTable(config, new);
+end
+
+local function registerWithHandyNotes ()
+  local defaults = {
+    icon_scale = 1,
+    icon_alpha = 1,
+  };
+
+  if (storedData == nil) then
+    storedData = {
+      settings = {};
+    };
+  elseif (storedData.settings == nil) then
+    storedData.settings = {};
+  end
+
+  settings = storedData.settings;
+  validateSettings(settings, defaults);
+
+  local options = {
+    type = "group",
+    name = 'Handynotes Pandaria',
+    desc = 'Handynotes Pandaria',
+    get = function(info) return settings[info.arg] end,
+    set = function(info, v)
+      settings[info.arg] = v
+    end,
+    args = {
+      desc = {
+        name = 'Handynotes Pandaria Options.',
+        type = "description",
+        order = 0,
+      },
+      icon_scale = {
+        type = 'range',
+        name = 'Icon Scale',
+        desc = 'The scale of the icons',
+        min = 0.25, max = 2, step = 0.01,
+        arg = "icon_scale",
+        order = 10,
+      },
+      icon_alpha = {
+        type = 'range',
+        name = 'Icon Alpha',
+        desc = 'The alpha transparency of the icons',
+        min = 0, max = 1, step = 0.01,
+        arg = "icon_alpha",
+        order = 20,
+      },
     },
-    icon_scale = {
-      type = 'range',
-      name = 'Icon Scale',
-      desc = 'The scale of the icons',
-      min = 0.25, max = 2, step = 0.01,
-      arg = "icon_scale",
-      order = 10,
-    },
-    icon_alpha = {
-      type = 'range',
-      name = 'Icon Alpha',
-      desc = 'The alpha transparency of the icons',
-      min = 0, max = 1, step = 0.01,
-      arg = "icon_alpha",
-      order = 20,
-    },
-  },
-};
+  };
+
+  HandyNotes:RegisterPluginDB(addonName, handler, options);
+end
 
 local function updateNodes ()
   HandyNotes:SendMessage('HandyNotes_NotifyUpdate', addonName);
 end
 
 addon:on('PLAYER_LOGIN', function ()
-  HandyNotes:RegisterPluginDB(addonName, handler, options);
+  registerWithHandyNotes();
   addon:funnel({'CRITERIA_UPDATE'}, 2, updateNodes);
   addon:on({'NEW_TOY_ADDED', 'NEW_MOUNT_ADDED'}, updateNodes);
 end);
